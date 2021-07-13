0ab09a51d70b828507f36f720d04d6e1046fae1d
TonyJem <a.jemanov@gmail.com>
2021-07-13 22:26:51 +0300

    Start TableView's setup

--- VideoGalery/VideoGalery/ThumbnailViewController.swift
+++ VideoGalery/VideoGalery/ThumbnailViewController.swift
@@ -1,10 +1,32 @@
 import UIKit
 
-class ThumbnailViewController: UIViewController {
-
+final class ThumbnailViewController: UIViewController {
+    @IBOutlet private weak var thumbnailsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Funny videos"
+        
+        thumbnailsTableView.backgroundColor = .systemGray5
+        thumbnailsTableView.tableFooterView = UIView()
+        thumbnailsTableView.dataSource = self
+        thumbnailsTableView.delegate = self
+    }
+}
+
+// MARK: - TableView DataSource
+extension ThumbnailViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
+
+// MARK: - TableView Delegate
+extension ThumbnailViewController: UITableViewDelegate {
 }
