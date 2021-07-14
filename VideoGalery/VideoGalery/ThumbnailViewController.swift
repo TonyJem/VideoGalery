8e70f7c84c797c3499ec63a513d46e98c7e1e3f7
TonyJem <a.jemanov@gmail.com>
2021-07-14 21:17:25 +0300

    Update ThumbnailViewController.swift

--- VideoGalery/VideoGalery/ThumbnailViewController.swift
+++ VideoGalery/VideoGalery/ThumbnailViewController.swift
@@ -3,6 +3,8 @@ import UIKit
 final class ThumbnailViewController: UIViewController {
     @IBOutlet private weak var thumbnailsTableView: UITableView!
     
+    private let videoThumbnailNames = ["video1", "video2", "video3", "video4"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,12 +23,13 @@ final class ThumbnailViewController: UIViewController {
 // MARK: - TableView DataSource
 extension ThumbnailViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return videoThumbnailNames.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         guard let cell = thumbnailsTableView.dequeueReusableCell(withIdentifier: String(describing: ThumbnailTableViewCell.self), for: indexPath) as? ThumbnailTableViewCell else {
+            
             return UITableViewCell()
         }
         
@@ -37,4 +40,12 @@ extension ThumbnailViewController: UITableViewDataSource {
 
 // MARK: - TableView Delegate
 extension ThumbnailViewController: UITableViewDelegate {
+}
+
+// MARK: - Private Methods
+private extension ThumbnailViewController {
+    
+    func showImage(with name: String) -> UIImage {
+        return #imageLiteral(resourceName: name)
+    }
 }
