209fe5870530cddd75b5eab802e54f54f7cea991
TonyJem <a.jemanov@gmail.com>
2021-07-24 08:34:17 +0300

    Refactor model

--- VideoGalery/VideoGalery/View/ThumbnailViewController.swift
+++ VideoGalery/VideoGalery/View/ThumbnailViewController.swift
@@ -4,7 +4,9 @@ import UIKit
 final class ThumbnailViewController: UIViewController {
     @IBOutlet private weak var tableView: UITableView!
     
-    private let videoThumbnailNames = ["video1", "video2", "video3", "video4"]
+    lazy var videos: [Video] = {
+        return Video.fetchVideos()
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +26,7 @@ final class ThumbnailViewController: UIViewController {
 // MARK: - TableView DataSource
 extension ThumbnailViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return videoThumbnailNames.count
+        return videos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -32,7 +34,7 @@ extension ThumbnailViewController: UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: ThumbnailTableViewCell.self), for: indexPath) as? ThumbnailTableViewCell else {
             return UITableViewCell()
         }
-        cell.fillCell(with: showImage(videoThumbnailNames[indexPath.row]))
+        cell.fillCell(with: videos[indexPath.row].videoImage)
         return cell
     }
     
@@ -40,12 +42,4 @@ extension ThumbnailViewController: UITableViewDataSource {
 
 // MARK: - TableView Delegate
 extension ThumbnailViewController: UITableViewDelegate {
-}
-
-// MARK: - Private Methods
-private extension ThumbnailViewController {
-    
-    func showImage(_ name: String) -> UIImage {
-        return #imageLiteral(resourceName: name)
-    }
 }
