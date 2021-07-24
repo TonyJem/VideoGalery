9e241cae92f8d446ea187884f34db9a066153536
TonyJem <a.jemanov@gmail.com>
2021-07-24 12:08:11 +0300

    Add extension to generate Thumbnail for selected URL

---
+++ VideoGalery/VideoGalery/Helpers/URL+Extension.swift
@@ -0,0 +1,19 @@
+import UIKit
+import AVFoundation
+
+extension URL {
+    func getThumbnailImage() -> UIImage {
+        let asset: AVAsset = AVAsset(url: self)
+        let imageGenerator = AVAssetImageGenerator(asset: asset)
+        
+        do {
+            let thumbnailImage = try imageGenerator.copyCGImage(at: CMTimeMake(value: 1, timescale: 60) , actualTime: nil)
+            return UIImage(cgImage: thumbnailImage)
+        } catch let error {
+            print("🔴 \(error)")
+        }
+        
+        // TODO: insert defaultImage Picture here and to assets
+        return #imageLiteral(resourceName: "video01")
+    }
+}
